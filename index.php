e45d7cb0e8331c5407731c335bc67f3c4af6f847
killianmurnane <23368888@studentmail.ul.ie>
2026-02-17 16:07:38 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <!-- Misc -->
+         <title>Skill Swap</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Welcome to Skill Swap!</h1>
+            <p>Click the button below to log in with your Google account.</p>
+            <a href="login.php" class="btn btn-primary">Log in with Google</a>
+        </div>
+    </body>
+</html>
